7f40f01d1c3eb8bd866812d8ef4f7ac6aaf4476c
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-14 17:39:01 -0500

    how 2 gatsby

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -8,5 +8,8 @@ export default () => (
     <h1>Hello world!</h1>
     <h2>Welcome to Brians Gatsby Site</h2>
     <h3>roaring like a flapper 100 years ago</h3>
+    <h6 style={{ color: `green` }}>
+      ! just gotta add some stuff that's not plain old text !
+    </h6>
   </div>
 )
